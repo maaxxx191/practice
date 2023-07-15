b5e922f15519ad48bc005b51d25b6f996befd927
maaxxx191 <maam96@icloud.com>
2023-07-16 03:35:05 +0900

    認証機能追加

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,11 @@
 |
 */
 
-Route::get('top', 'TopController@topView'); 
+Route::get('/', function() { 
+  return view('welcome');
+}); 
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
